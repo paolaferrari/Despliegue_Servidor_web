9fca76750f54f498a291e28aca6ea3f929a85f3b
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-12 21:40:31 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "El directorio es : "
-cd /home/Sites/sarapico
+#cd /home/Sites/sarapico
 pwd
 
-echo "Usuario logeado como : "
-whoami
+#echo "Usuario logeado como : "
+#whoami
 
 #Creamos imagenes docker --> debian y sarapico 
-sudo docker build -t sarapico .
+#sudo docker build -t sarapico .
 
 #Creamos docker sarapico
-sudo docker run --rm -h sarapico.com -p  4000:80 sarapico
+#sudo docker run --rm -h sarapico.com -p  4000:80 sarapico
