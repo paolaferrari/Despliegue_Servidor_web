3d273807779905d337cd4c536502e2bcc1240b45
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-14 14:30:51 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "El directorio es : "
+cd /Sites/sarapico
+pwd
+
+echo "Usuario logeado como : "
+whoami
+
+#Creamos imagenes docker --> debian y sarapico 
+sudo docker build -t sarapico .
+
+#Creamos docker sarapico
+sudo docker run --rm -h sarapico.com -p  4000:80 sarapico
