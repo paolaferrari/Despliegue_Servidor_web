3922ad12feadf79c4ba704b05be8e56526d8fb90
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-15 20:58:22 -0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -17,7 +17,7 @@ echo "Usuario logeado como : "
 whoami
 
 echo "Creamos imagenes docker --> debian y sarapico" 
-sudo ~/Sites/sarapico/docker build -t sarapico .
+su ~/Sites/sarapico/docker build -t sarapico .
 
 #echo devops | sudo -S -u devops  ~/Sites/sarapico/docker build -t sarapico .
 
