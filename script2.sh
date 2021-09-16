fb5a88b56662772c2d178ef0c0539cad17e54553
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-15 22:26:59 -0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,21 +6,17 @@ echo "Usuario logeado como : "
 whoami
 
 #ssh devops@192.168.1.117 'bash -s' < script.sh
-#whoami
-
 #ssh devops@192.168.1.118 
 #sshpass -p devops ssh devops@192.168.1.118 'bash -s' < script2.sh
+
 cd /home/devops/Sites/sarapico
 pwd
-
-echo "Usuario logeado como : "
-whoami
 
 echo "Creamos Imagenes de docker --> debian y sarapico" 
 
 docker build -t sarapico .
 
-
 echo "Creamos docker sarapico"
 docker run --rm -h sarapico.com -p 33:22 -p  4000:80 sarapico
+
 exit
