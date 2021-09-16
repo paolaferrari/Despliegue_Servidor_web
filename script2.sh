9bc181a0af6557b14f0017dfe909732d32f9323d
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-15 21:27:34 -0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -20,12 +20,6 @@ echo "Creamos Imagenes de docker --> debian y sarapico"
 
 docker build -t sarapico .
 
-#echo devops | sudo -S -u devops /home/Sites/sarapico/docker build -t sarapico .
 
-echo "verificar"
-pwd
-whoami
-
-
-#Creamos docker sarapico
-#sudo docker run --rm -h sarapico.com -p 33:22 -p  4000:80 sarapico
+echo "Creamos docker sarapico y desplegamos"
+docker run --rm -h sarapico.com -p 33:22 -p  4000:80 sarapico
