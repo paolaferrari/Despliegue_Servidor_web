48c57affab06419e562598f74c0434af4e8fc15b
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-16 21:49:04 -0300

    Update scriptWinaLin.sh

--- scriptWinaLin.sh
+++ scriptWinaLin.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 # Transferencia desde Git Local a Servidor Web remoto
-cd /home/devops/Sites/sarapico
+#cd /home/devops/Sites/sarapico
 echo "El directorio es : "
 pwd
 echo "Usuario logeado como : "
 whoami
-
-scp -rp devops@192.168.1.118:~/Sites/sarapico 192.168.1.100:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
+#desde linux
+#scp -rp devops@192.168.1.118:~/Sites/sarapico 192.168.1.100:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
+# desde windows
+scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh devops@192.168.1.118:~/Sites/sarapico
