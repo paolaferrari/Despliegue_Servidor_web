1285e101d91d92b4173e441677b6ab10cbba20b2
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-16 21:31:58 -0300

    Update scriptWinaLin.sh

--- scriptWinaLin.sh
+++ scriptWinaLin.sh
@@ -6,4 +6,4 @@ pwd
 echo "Usuario logeado como : "
 whoami
 
-scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh devops@192.168.1.118:~/Sites/sarapico
+scp -rp devops@192.168.1.118:~/Sites/sarapico 192.168.1.100:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
