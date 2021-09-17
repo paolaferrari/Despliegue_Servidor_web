882aa78138f7e2245c880f63dcccb95c024151f5
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-16 21:20:49 -0300

    Update scriptWinaLin.sh

--- scriptWinaLin.sh
+++ scriptWinaLin.sh
@@ -1,6 +1,4 @@
 #!/bin/bash 
-# Aquí ponemos los datos de conexión
-
+# Transferencia desde Git Local a Servidor Web remoto
 
 scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh devops@192.168.1.118:~/Sites/sarapico
-
