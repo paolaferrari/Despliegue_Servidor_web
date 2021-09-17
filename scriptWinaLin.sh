4aba9e7670b61476451b853abe5839a2c87b6cf8
paolaferrari <55800303+paolaferrari@users.noreply.github.com>
2021-09-17 11:46:15 -0300

    Update scriptWinaLin.sh

--- scriptWinaLin.sh
+++ scriptWinaLin.sh
@@ -6,6 +6,6 @@ pwd
 echo "Usuario logeado como : "
 whoami
 #desde linux
-#scp -rp devops@192.168.1.118:~/Sites/sarapico 192.168.1.100:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
+scp -rp pao@192.168.56.103:~/Sites/sarapico 192.168.56.103:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
 # desde windows
-scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh -p devops devops@192.168.1.118:~/Sites/sarapico
+#scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh -p devops devops@192.168.1.118:~/Sites/sarapico
