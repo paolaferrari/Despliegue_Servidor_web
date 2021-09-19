62327cf9677f5353d44016d5676b442b704738f4
paolaferrari <55800303+paolaferrari@users.noreply.github.com>
2021-09-19 12:17:33 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 
 # Detenemos y Eliminamos todos los contenedores creados
 cd /home/devops/Sites/sarapico
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
 
 #Eliminamos las imagenes debian y sarapico creadas
-docker rmi debian
-docker rmi sarapico
+sudo docker rmi debian
+sudo docker rmi sarapico
