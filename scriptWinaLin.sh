659e5cced93825ec0af9544b9bcb49ce9ee5eae8
paolaferrari <55800303+paolaferrari@users.noreply.github.com>
2021-09-22 21:07:31 -0300

    Update scriptWinaLin.sh

--- scriptWinaLin.sh
+++ scriptWinaLin.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 # Transferencia desde Git Local a Servidor Web remoto
 #cd /home/devops/Sites/sarapico
-echo "El directorio es : "
-pwd
-echo "Usuario logeado como : "
-whoami
+# echo "El directorio es : "
+# pwd
+#echo "Usuario logeado como : "
+# whoami
 #desde linux
-scp -rp pao@192.168.56.103:~/Sites/sarapico 192.168.56.103:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
+# scp -rp pao@192.168.56.103:~/Sites/sarapico 192.168.56.103:D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh 
 # desde windows
 #scp -rp D:\git_grandys\dockerfiletareagrupal\Despliegue_Servidor_web\scriptWinaLin.sh -p devops devops@192.168.1.118:~/Sites/sarapico
